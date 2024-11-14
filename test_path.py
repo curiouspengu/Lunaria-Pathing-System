bf4e917d239966f7cd0741d4eabd49be84eaf5f6
Curious Pengu <113814500+curiouspengu@users.noreply.github.com>
2024-11-14 20:00:04 +1100

    BUG FIXES

--- test_path.py
+++ test_path.py
@@ -1,7 +1,5 @@
 import sys
-import pathlib
 sys.dont_write_bytecode = True
-sys.path.append(pathlib.Path(__file__).parent.resolve())
 
 import os
 import subprocess
@@ -14,5 +12,4 @@ from settings import *
 focus_roblox()
 print("started")
 exec(open("paths.py"))
-
 print("finished")
