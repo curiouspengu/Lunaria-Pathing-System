5e3ebc98aa14d718c95f84a07c5a08b8cce3b15f
Curious Pengu <113814500+curiouspengu@users.noreply.github.com>
2024-11-14 18:36:46 +1100

    Bug Fixes

--- path.py
+++ path.py
@@ -1,1 +1,11 @@
 
+
+# New Recording
+walk_sleep('0.519')
+walk_send('d', 'Down')
+walk_sleep('0.165')
+walk_send('space', 'Down')
+walk_sleep('0.129')
+walk_send('space', 'Up')
+walk_sleep('0.441')
+walk_send('d', 'Up')
