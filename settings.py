0624b74fd8baff66156a114ed718bee4ab43e3ed
Curious Pengu <113814500+curiouspengu@users.noreply.github.com>
2024-11-18 09:35:33 +1100

    LPS Bug Fix

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 # Settings
 developer_walk_speed = 1.25
-player_walk_speed = 1 # vip or not
 
 
 
@@ -58,7 +57,7 @@ azerty_replace_dict = {"w":"z", "a":"q"}
 ahk = AHK()
 
 def walk_time_conversion(d):
-    final_walk_time = float(d) * (1.0 + (developer_walk_speed - 1.0) * (1 - player_walk_speed))
+    final_walk_time = float(d)
     return final_walk_time
 
 def walk_sleep(d):
