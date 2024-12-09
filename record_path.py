e3f8c6590245770f7bc4815a03538661ece08d9c
Curious Pengu <113814500+curiouspengu@users.noreply.github.com>
2024-12-10 10:34:20 +1100

    testing

--- record_path.py
+++ record_path.py
@@ -26,6 +26,7 @@ class Path():
         self.stop_replay_flag = False
 
         self.actions = []
+        self.keys_pressed = {}
 
         self.mouse_controller = mouse.Controller()
         self.keyboard_controller = keyboard.Controller()
@@ -42,35 +43,32 @@ class Path():
             keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as keyboard_listener:
             
             while not self.stop_recording_flag: time.sleep(0.1)
-
-            mouse_listener.stop()
             keyboard_listener.stop()
         
         self.recording = False
         self.actions[1] == ""
         self.save_recording()
 
-    def on_click(self, x, y, button, pressed):
-        self.record_mouse(x, y, button, pressed)
-
     def on_press(self, key):
+        if key in self.keys_pressed.keys():
+            if self.keys_pressed[key] == True:
+                return
+        
+        self.keys_pressed[key] = True
+
         if key == keyboard.Key.f2:
             self.stop_recording_flag = True
         self.record_keyboard(key, True)
 
     def on_release(self, key):
+        if key in self.keys_pressed.keys():
+            if self.keys_pressed[key] == False:
+                return
+        else:
+            return
+        
+        self.keys_pressed[key] = False
         self.record_keyboard(key, False)
-    
-    def record_mouse(self, x, y, button, pressed):
-        if not self.stop_recording_flag:
-            if not self.start_time:
-                self.start_time = time.time()
-            if not self.last_action:
-                self.last_action = self.start_time
-            
-            timestamp = time.time() - self.last_action
-            self.last_action = time.time()
-            self.actions.append(f"sleep({timestamp})\nahk.click()\n")
     
     def record_keyboard(self, key, pressed):
         if not self.stop_recording_flag:
@@ -79,11 +77,21 @@ class Path():
             if not self.last_action:
                 self.last_action = self.start_time
             
+            for key_status in self.keys_pressed:
+                if key_status.first == key:
+                    continue
+                if key_status.second == False:
+                    no_keys_pressed = True
+            
             key = self.convert_key_layout(str(key).replace("'", ""))
             timestamp = time.time() - self.last_action
             self.last_action = time.time()
             key = f'\"{key}\"' if "Key" not in key else key
-            self.actions.append(f"walk_sleep({timestamp})\nwalk_send({key}, {pressed})\n")
+
+            if no_keys_pressed == True:
+                self.actions.append(f"walk_sleep(0.1)\nwalk_send({key}, {pressed})\n")
+            else:
+                self.actions.append(f"walk_sleep({timestamp})\nwalk_send({key}, {pressed})\n")
     
     def convert_key_layout(self, key):
         if self.azerty_keyboard:
