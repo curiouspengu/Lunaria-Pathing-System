c3121d9f803f05cc510d4b088b6b65866471221d
Curious Pengu <113814500+curiouspengu@users.noreply.github.com>
2024-12-12 22:37:41 +1100

    v2.2

--- auto.py
+++ auto.py
@@ -35,13 +35,16 @@ ahk.add_hotkey("F1", exit_program)
 ahk.start_hotkeys()
 
 def align_camera():
-    focus_roblox()
-    # click_menu_button(2)
-    # sleep(0.1)
-    r_pos = get_roblox_window_pos()
-    # ahk.mouse_move(381 * (r_pos.width/1920.0), 143 * (r_pos.height/1080.0))
+    r_pos = window.get_roblox_window_pos()
+
+    pathlib.reset()
+    sleep(0.1)
+    click_menu_button(2)
+    sleep(0.1)
     
-    # ahk.click()
+    kc.tap("\\")
+    sleep(0.1)
+    kc.tap(Key.enter)
     sleep(0.1)
     ahk.mouse_drag(button="R", from_position=[r_pos.x + r_pos.width*0.2, r_pos.y + 44 + r_pos.height*0.05], x=r_pos.x + r_pos.width*0.2, y=r_pos.y + 400 + r_pos.height*0.05, send_mode="Input", speed=1)
     sleep(0.1)
@@ -51,17 +54,30 @@ def align_camera():
     for i in range(15):
         ahk.click(button="WD")
         sleep(0.01)
+    kc.tap("\\")
+
 
 def click_menu_button(button_num):
-    rel_pos = get_roblox_window_pos()
-    menu_button_spacing = 54 * (rel_pos.height/1080)
-    menu_button_width = 64 * (rel_pos.width/1920)
+    # rel_pos = window.get_roblox_window_pos()
+    # menu_button_spacing = 54 * (rel_pos.height/1080)
+    # menu_button_width = 64 * (rel_pos.width/1920)
 
-    start_y = 367 * (rel_pos.height/1080)
-    start_x = 11 * (rel_pos.width/1920)
+    # start_y = 367 * (rel_pos.height/1080)
+    # start_x = 11 * (rel_pos.width/1920)
 
-    ahk.mouse_move(x = start_x + (int(menu_button_width/2)), y = start_y + (menu_button_spacing * button_num))
-    ahk.click()
+    # ahk.mouse_move(x = start_x + (int(menu_button_width/2)), y = start_y + (menu_button_spacing * button_num))
+    # ahk.click()
+
+    kc.tap('\\')
+    for i in range(4):
+        sleep(0.1)
+        kc.tap(Key.left) 
+    for i in range(5 - button_num):
+        sleep(0.1)
+        kc.tap(Key.up)
+    sleep(0.1)
+    kc.tap(Key.enter)
+    kc.tap('\\')
     
 def windowEnumerationHandler(hwnd, top_windows):
     top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
